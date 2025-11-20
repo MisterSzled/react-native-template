7718b3fcd1b08008b9e93adf38cbdb780763360d
MisterSzled <greg.baker2@googlemail.com>
2025-11-20 13:02:09 +0000

    init gluestack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,9 @@
 import "../global.css";
 import { Stack } from "expo-router";
 
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+
 export default function RootLayout() {
         return <Stack />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
-import { Text, View } from "react-native";
+import { Box } from "@/components/ui/box";
+import { Text } from "@/components/ui/text";
 
 export default function Index() {
         return (
-                <View
-                        style={{
-                                flex: 1,
-                                justifyContent: "center",
-                                alignItems: "center",
-                        }}
+                <Box
+                        className="flex justify-center items-center h-full"
                 >
-                        <Text className="text-red-500">Edit app/index.tsx to edit this screen.</Text>
-                </View>
+                        <Text className="text-red-500">Words</Text>
+                </Box>
         );
 }
