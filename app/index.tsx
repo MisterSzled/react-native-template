41072182e2cda040b9f94b61b238192164b6b8fd
MisterSzled <greg.baker2@googlemail.com>
2025-11-20 18:52:37 +0000

    jest and gluestack finally working

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,15 @@
-// import { Box } from "@/components/ui/box";
-// import { Text } from "@/components/ui/text";
+import { Box } from "@/components/ui/box";
+import { Text } from "@/components/ui/text";
 import { useTranslation } from "react-i18next";
-import { View, Text } from "react-native";
 
 const index = () => {
         const { t } = useTranslation();
         return (
-                // <Box
-                //         className="flex justify-center items-center h-full"
-                // >
-                //         <Text className="text-red-500">{t("welcome")}</Text>
-                // </Box>
-
-                <View>
+                <Box
+                        className="flex justify-center items-center h-full"
+                >
                         <Text className="text-red-500">{t("welcome")}</Text>
-                </View>
+                </Box>
         )
 }
 
