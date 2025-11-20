afb7bf6609dee2c72fde3df563959451f877caba
MisterSzled <greg.baker2@googlemail.com>
2025-11-20 21:15:19 +0000

    clean config files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,13 @@
 import '@/global.css';
 import { Stack } from "expo-router";
 
-import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
-
 import { I18nextProvider } from 'react-i18next';
 import i18n from '@/src/i18n/index';
 
 import { QueryProvider } from "@/src/providers/query";
 import { useEffect, useState } from 'react';
 
-import { startMocks } from '@/msw/setup';
+import { startMocks } from '@/config/msw/setup';
 
 export default function RootLayout() {
         const [ready, setReady] = useState(!__DEV__);
