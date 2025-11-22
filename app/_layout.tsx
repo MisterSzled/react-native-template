635d2f84c5af94739335c7481ee81f8d4e97ddad
MisterSzled <greg.baker2@googlemail.com>
2025-11-22 16:07:55 +0000

    tamagui and root layout cleanup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,20 @@
 import { Stack } from "expo-router";
 
-import { I18nextProvider } from 'react-i18next';
-import i18n from '@/src/i18n/index';
-
-import { useEffect, useState } from 'react';
-
-import { startMocks } from '@/_config/msw/setup';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import I18next from "@/src/providers/I18next";
+import Msw from "@/src/providers/Msw";
+import Query from "@/src/providers/Query";
+import Tamagui from "@/src/providers/Tamagui";
 
 export default function RootLayout() {
-        const queryClient = new QueryClient();
-        const [ready, setReady] = useState(!__DEV__);
-
-        useEffect(() => {
-                startMocks().then(() => setReady(true))
-        }, []);
-        
-        if (!ready) return null;
-
-        return <QueryClientProvider client={queryClient}>
-                <I18nextProvider i18n={i18n}>
-                        <Stack screenOptions={{
-                                headerShown: false
-                        }}/>
-                </I18nextProvider>
-        </QueryClientProvider>
+        return <Msw>
+                <Query>
+                        <Tamagui >
+                                <I18next >
+                                        <Stack screenOptions={{
+                                                headerShown: false
+                                        }} />
+                                </I18next>
+                        </Tamagui>
+                </Query>
+        </Msw>
 }
--- src/features/auth/screens/LoginScreen.tsx
+++ src/features/auth/screens/LoginScreen.tsx
@@ -1,6 +1,7 @@
 import { useProducts } from "@/src/api/product/product.hooks";
 import { useTranslation } from "react-i18next";
 import { View } from "react-native";
+import { Button } from "tamagui";
 
 const index = () => {
         const { t } = useTranslation();
@@ -11,7 +12,7 @@ const index = () => {
 
         return (
                 <View >
-                        
+                        <Button theme={"red"} width={"$10"} height={"$10"}/>
                 </View>
         )
 }
